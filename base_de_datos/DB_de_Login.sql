dd39aa289fa9de31cbdbddff9263861054833c44
funcions <107017204+funcions@users.noreply.github.com>
2023-11-13 08:42:02 -0500

    Actualizar DB_de_Login.sql

--- base_de_datos/DB_de_Login.sql
+++ base_de_datos/DB_de_Login.sql
@@ -1,18 +1,19 @@
--- nombre de la base de datos: login_Web
+-- Crear la base de datos y seleccionarla
 CREATE DATABASE game_data_base;
 USE game_data_base;
 
+-- Crear la tabla 'login'
 CREATE TABLE `login` (
   `id_login` int AUTO_INCREMENT PRIMARY KEY,
   `usuario` varchar(120),
   `password` varchar(120),
-  `email` varchar(120) 
-  
+  `email` varchar(120)
 );
 
+-- Crear la tabla 'anagrama' con una clave foránea que referencia a 'login'
 CREATE TABLE `anagrama` (
   `id_anagrama` int AUTO_INCREMENT PRIMARY KEY,
   `id_usuario` int,
-		`score` int
-
+  `score` int,
+  FOREIGN KEY (`id_usuario`) REFERENCES `login`(`id_login`)
 );
